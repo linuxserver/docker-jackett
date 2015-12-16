b4e48a9900e54f06eab43357df0f498db90e950f
sparklyballs <sparklyballs@gmail.com>
2015-12-16 02:11:22 +0000

    Update 30_update.sh

--- init/30_update.sh
+++ init/30_update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# determine latest remote version
 jack_ver=$(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest | grep browser_download_url | grep Mono | cut -d '"' -f 4)
 jackremote=$(echo $jack_ver | tr -d [=.=][:alpha:][=/=][=:=][:space:])
 
-# get local versin
+# get local version
 jacktest=$(mono /app/Jackett/JackettConsole.exe --version)
 jackcut1=$(echo $jacktest | tr -d [=.=][:alpha:][:space:])
 jacklocal=${jackcut1%0}
@@ -14,6 +15,7 @@ rm -rf /app/Jackett
 curl -o /tmp/jacket.tar.gz -L $jack_ver
 mkdir -p /app/Jackett
 tar xvf /tmp/jacket.tar.gz -C /app/Jackett --strip-components=1
+rm /tmp/jacket.tar.gz
 fi
 
 chown -R abc:abc /app
