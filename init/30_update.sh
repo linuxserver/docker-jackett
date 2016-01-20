4c36a9462e03679b7d893af1735c864fe5ebd82f
sparklyballs <sparklyballs@gmail.com>
2016-01-20 06:12:01 +0000

    shellcheck warnings

--- init/30_update.sh
+++ init/30_update.sh
@@ -2,11 +2,11 @@
 
 # determine latest remote version
 jack_ver="$(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest | grep browser_download_url | grep Mono | cut -d '"' -f 4)"
-jackremote="$(echo $jack_ver | tr -d "[=.=][:alpha:][=/=][=:=][:space:]")"
+jackremote="$(echo "$jack_ver" | tr -d "'=.='':alpha:''=/=''=:='':space:'")"
 
 # get local version
 jacktest="$(mono /app/Jackett/JackettConsole.exe --version)"
-jackcut1="$(echo $jacktest | tr -d "[=.=][:alpha:][:space:]")"
+jackcut1="$(echo "$jacktest" | tr -d "'=.='':alpha:'':space:'")"
 jacklocal=${jackcut1%0}
 
 # test if we need to update
