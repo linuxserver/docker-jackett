637c6be666803a785be5ac6978f95fdbbc576c91
sparklyballs <sparklyballs@gmail.com>
2016-01-23 13:00:35 +0000

    strip non numerical characters during version compare test

--- init/30_update.sh
+++ init/30_update.sh
@@ -5,18 +5,19 @@
 
 
 # determine latest remote version
-jackremote="$(wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}')"
+jack_remote="$(wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep -E \/tag\/ | awk -F "[><]" '{print $3}')"
 
 # get local version
-jacklocal="$(mono /app/Jackett/JackettConsole.exe --version | sed -e 's/.*t//' -e "s/\.0.*//")"
+jack_local="$(mono /app/Jackett/JackettConsole.exe --version | sed -e 's/.*t//' -e "s/\.0.*//")"
 
 # test if we need to update
-if [ "$jackremote" -gt "$jacklocal" ]; then
+if [ "${jack_remote//[!0-9]/}" -gt "${jack_local//[!0-9]/}" ]; then
 rm -rf /app/Jackett
-curl -o /tmp/jacket.tar.gz -L "$jack_ver"
+curl -o /tmp/jacket.tar.gz -L https://github.com/Jackett/Jackett/releases/download/"$jack_remote"/Jackett.Binaries.Mono.tar.gz
 mkdir -p /app/Jackett
 tar xvf /tmp/jacket.tar.gz -C /app/Jackett --strip-components=1
 rm /tmp/jacket.tar.gz
 fi
 
 chown -R abc:abc /app
+
